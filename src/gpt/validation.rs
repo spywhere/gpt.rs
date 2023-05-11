8b6c3421f93f0cbcec74b24fbb294708a55a858a
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-05-12 02:45:06 +0700

    feat: add more validation

--- src/gpt/validation.rs
+++ src/gpt/validation.rs
@@ -1,9 +1,32 @@
+use atty;
 use crate::cli::options::GptOptions;
 use crate::cli::Error;
 
 pub(super) fn validate(opts: &GptOptions) -> Result<(), Error> {
+  validate_api_key(opts)?;
+  validate_context(opts)?;
+  validate_prompt(opts)?;
+
+  Ok(())
+}
+
+fn validate_api_key(opts: &GptOptions) -> Result<(), Error> {
   match opts.envs.api_key {
     Some(_) => Ok(()),
     None => Err(Error { exit_code: 1, message: "OPENAPI_API_KEY is required" })
+  }
+}
+
+fn validate_context(opts: &GptOptions) -> Result<(), Error> {
+  match (atty::is(atty::Stream::Stdout), &opts.flags.context) {
+    (true, Some(_)) => Err(Error { exit_code: 1, message: "context cannot be used in the pipe" }),
+    (_, _) => Ok(())
+  }
+}
+
+fn validate_prompt(opts: &GptOptions) -> Result<(), Error> {
+  match (&opts.flags.context, opts.prompt.join(" ").as_str()) {
+    (None, "") => Err(Error { exit_code: 0, message: "No prompt given\nUse --help for usage" }),
+    (_, _) => Ok(())
   }
 }
