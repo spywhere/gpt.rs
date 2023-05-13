e9b022f8b374591f3b4fc8fdfd36edfd007da094
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-05-14 04:39:24 +0700

    feat: support basic chat completions prompt

--- src/gpt/mod.rs
+++ src/gpt/mod.rs
@@ -60,6 +60,22 @@ pub fn prompt(opts: &GptOptions, prompt: &String) -> Result<(), Exit> {
 
   println!("Is TTY: {}", atty::is(atty::Stream::Stdin));
 
+  println!("Prompt: {}", prompt);
+
+  let mut messages: Vec<openai::model::Message> = Vec::new();
+
+  messages.push(openai::model::Message {
+    role: openai::model::Role::User,
+    content: prompt.to_string()
+  });
+
+  let request = openai::model::ChatCompletions {
+    model: opts.flags.model.clone(),
+    max_tokens: opts.flags.max_tokens,
+    temperature: opts.flags.temperature,
+    messages
+  };
+
   let openai = openai::OpenAi::new(
     opts.envs.api_host.to_string(),
     opts.envs.api_key.clone().unwrap().to_string(),
@@ -67,7 +83,14 @@ pub fn prompt(opts: &GptOptions, prompt: &String) -> Result<(), Exit> {
     opts.debug || opts.debug_dry,
     opts.debug_dry
   );
-  let models = openai.models().map_err(to_exit)?;
+  let response = openai.chat_completions(&request).map_err(to_exit)?;
+
+  let messages: Vec<String> = response.choices
+    .into_iter()
+    .map(|choice| choice.message.content)
+    .collect();
+
+  println!("{}", messages.join("\n"));
 
   Ok(())
 }
